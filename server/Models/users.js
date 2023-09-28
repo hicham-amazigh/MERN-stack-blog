a399f14a50787e2f1b833c40ee86c9d737fdb532
hichamAam <hicham.am19999@gmail.com>
2023-09-28 18:18:27 +0100

    First commit

---
+++ server/Models/users.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const UserSchema = new mongoose.Schema({
+    username:{
+        type: String,
+    },
+    email:{
+        type: String,
+    },
+    password:{
+        type: String,
+    },
+})
+
+const UserModel = mongoose.model("users", UserSchema)
+module.exports = UserModel
---
+++ server/server.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const Port = "5000"
+
+
+// DB connection
+const mongoose = require("mongoose")
+try {
+    mongoose.connect("mongodb+srv://hamazigh:RdtWzQKoyq5ycL9O@cluster0.wig74am.mongodb.net/Mern?retryWrites=true&w=majority")
+} catch (error) {
+    console.log(error)
+}
+
+// Import models
+const UserModel = require('./Models/users')
+const { JsonWebTokenError } = require('jsonwebtoken')
+
+
+app.get('/users', async (req, res)=>{
+    const Users = await UserModel.find()
+    res.json(Users)
+})
+
+
+app.get('/register', async (req, res)=>{
+    
+})
+
+app.post('/user/add', async (req, res)=>{
+    const {username, email, password} = req.body
+    const user = await UserModel.findOne({username})
+    if(user){
+        res.send("user already exist !")
+    }else{
+        const newuser = new UserModel({ username, email,
+            password: bcrypt.hashSync(password, 10)
+        })
+        await newuser.save();
+    }
+    res.send('bien ajouter')
+})
+
+app.get('/Login', async (req, res)=>{
+    const {username, password} = req.body
+    const user = await UserModel.findOne({username})
+    if(!user){
+        res.json('user not found !')
+    }else{
+        const isPassValid = await bcrypt.compare(password, user.password)
+        if(isPassValid){
+            res.send("login sucsess !")
+        }else{
+            res.send("Password incorecct !")
+        }
+    }
+    const token = jwt.sign({id: user._id}, process.env.SECRET)
+})
+
+
+app.get('/home', (req, res)=>{
+    res.send("welcome to home page !")
+})
+
+
+app.listen(Port , ()=>{
+    console.log("hello to hicham amazigh server !")
+})
